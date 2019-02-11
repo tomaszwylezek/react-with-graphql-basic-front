e212cee885c086a755e85f7449c5c5f6c2ba8553
Tomasz Wylężek <tomwylezek@gmail.com>
2019-02-11 17:51:08 +0100

    Add static LyricList

---
+++ client/components/LyricList.js
@@ -0,0 +1,15 @@
+import React, { Component} from 'react';
+
+class LyricList extends  Component{
+  render() {
+    return (
+
+      <ul>
+        <li>babla</li>
+      </ul>
+    )
+  }
+}
+
+
+export default LyricList
--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -4,6 +4,7 @@ import { Link } from "react-router";
 
 import fetchSong from "../queries/fetchSong";
 import LyricCreate from "./LyricCreate";
+import LyricList from "./LyricList";
 
 class SongDetail extends Component {
   render() {
@@ -17,7 +18,8 @@ class SongDetail extends Component {
       <div>
         <Link to="/">Back</Link>
         <h3>{song.title}</h3>
-        <LyricCreate songId={this.props.params.id}/>
+        <LyricList />
+        <LyricCreate songId={this.props.params.id} />
       </div>
     );
   }
