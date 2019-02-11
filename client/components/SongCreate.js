a08e4c1e1d01dcd51a6209fd1c633aff815d7507
Tomasz Wylężek <tomwylezek@gmail.com>
2019-02-11 15:03:45 +0100

    Refetch data after add song

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -3,6 +3,8 @@ import { graphql } from "react-apollo";
 import gql from "graphql-tag";
 import { Link, hashHistory } from "react-router";
 
+import query from "../queries/fetchSongs";
+
 class SongCreate extends Component {
   constructor(props) {
     super(props);
@@ -19,7 +21,8 @@ class SongCreate extends Component {
       .mutate({
         variables: {
           title: this.state.title
-        }
+        },
+        refetchQueries: [{ query }]
       })
       .then(() => {
         hashHistory.push("/");
--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import gql from "graphql-tag";
 import { graphql } from "react-apollo";
 import { Link } from "react-router";
+
+import query from "../queries/fetchSongs";
 
 class SongList extends Component {
   renderSongs() {
@@ -27,13 +28,5 @@ class SongList extends Component {
     );
   }
 }
-
-const query = gql`
-  {
-    songs {
-      title
-    }
-  }
-`;
 
 export default graphql(query)(SongList);
