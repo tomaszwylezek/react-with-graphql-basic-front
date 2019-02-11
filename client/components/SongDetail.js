642a0f69b0ffaa45594c07ad7a4e0e3b649db3c6
Tomasz Wylężek <tomwylezek@gmail.com>
2019-02-11 17:29:44 +0100

    Fetch detail of song

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { graphql } from "react-apollo";
+
+import fetchSong from "../queries/fetchSong";
 
 class SongDetail extends Component {
   render() {
@@ -10,4 +13,10 @@ class SongDetail extends Component {
   }
 }
 
-export default SongDetail;
+export default graphql(fetchSong, {
+  options: ({ params: { id } }) => ({
+    variables: {
+      id
+    }
+  })
+})(SongDetail);
