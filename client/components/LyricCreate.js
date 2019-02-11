f3a52530b23e79345a30e13f69dbc38875d6147b
Tomasz Wylężek <tomwylezek@gmail.com>
2019-02-11 17:49:12 +0100

    Handle submit lyrics

--- client/components/LyricCreate.js
+++ client/components/LyricCreate.js
@@ -1,14 +1,62 @@
 import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
+import query from "../queries/fetchSongs";
+import { hashHistory } from "react-router";
 
 class LyricCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      content: ""
+    };
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+
+    this.props
+      .mutate({
+        variables: {
+          content: this.state.content,
+          songId: this.props.songId
+        }
+      })
+      .then(() => {
+        this.setState({
+          content: ""
+        });
+      });
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.onSubmit.bind(this)}>
         <label>Add a Lyric</label>
-        <input type="text" name="" id="" />
+        <input
+          type="text"
+          value={this.state.content}
+          onChange={event =>
+            this.setState({
+              content: event.target.value
+            })
+          }
+        />
       </form>
     );
   }
 }
 
-export default LyricCreate;
+const mutation = gql`
+  mutation AddLyricToSong($content: String, $songId: ID) {
+    addLyricToSong(content: $content, songId: $songId) {
+      id
+      lyrics {
+        content
+      }
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricCreate);
